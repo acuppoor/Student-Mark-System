0be575dfab55ec3871f8813313c03335e02bf0d4
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2017-08-25 08:07:29 +0200

    nodejs has been added to the project

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return view('auth');
+});
+
+Route::post('/user', function () {
+    return view('welcome');
+});
